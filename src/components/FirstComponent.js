25900b5fbc7aeb17cbc96fcfcd306f36acd293d2
jtx007 <jamesjacobthomas7@gmail.com>
2018-10-27 13:21:00 -0400

    background image and color scheme picked

---
+++ src/components/FirstComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class FirstComponent extends Component {
+    render () {
+        return (
+            <div className="top-home-div">
+                <p>James Thomas</p>
+                <p>Software Engineer | Full Stack Developer</p>
+            </div>
+        )
+    }
+}
