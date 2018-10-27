e0ec08257ea18a54b106fc4e403778ade94d6217
jtx007 <jamesjacobthomas7@gmail.com>
2018-10-27 15:29:53 -0400

    added avatar picture

--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import avatar from '../avatar.png'
 
 export default class FirstComponent extends Component {
     render () {
         return (
-            <div className="top-home-div">
-                <p>James Thomas</p>
-                <p>Software Engineer | Full Stack Developer</p>
+            <div className="outter-most-div">
+                <div className="top-home-div">
+                    <img className="avatar" src={avatar}/>
+                    <p>James Thomas</p>
+                    <p>Software Engineer | Full Stack Developer</p>
+                <div className="second-level-div">
+                    <p>Projects</p>
+                </div>
+                </div>
             </div>
         )
     }
