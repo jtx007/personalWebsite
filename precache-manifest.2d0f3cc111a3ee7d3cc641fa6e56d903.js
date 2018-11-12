16a245241cdcecd386ebef36e5e61ff43f74ea32
jtx007 <jamesjacobthomas7@gmail.com>
2018-11-11 19:17:35 -0500

    Updates

---
+++ precache-manifest.2d0f3cc111a3ee7d3cc641fa6e56d903.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5477dd5778a19c7ff2e2",
+    "url": "/static/css/main.c4bfcd18.chunk.css"
+  },
+  {
+    "revision": "5477dd5778a19c7ff2e2",
+    "url": "/static/js/main.5477dd57.chunk.js"
+  },
+  {
+    "revision": "9fefa229b7416825b89a",
+    "url": "/static/js/1.9fefa229.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a96b76cb292e22d9578b5a15da384d6",
+    "url": "/static/media/avatar.5a96b76c.png"
+  },
+  {
+    "revision": "9a41b226fdd9c455cceb04758b27b561",
+    "url": "/static/media/githubicon.9a41b226.png"
+  },
+  {
+    "revision": "b760a495a28015a4181c1d71ed12f1eb",
+    "url": "/index.html"
+  }
+];
