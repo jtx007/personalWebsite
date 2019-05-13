2d692373c288201ebee09cbfede4ad0d6ba34767
jtx007 <jamesjacobthomas7@gmail.com>
2019-05-13 15:54:45 -0400

    edited spacing between project links

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -15,14 +15,14 @@ export default class ProjectContainer extends Component {
                                 <a href="https://connosieurfrontend.herokuapp.com/"><img className="project-picture" alt="demopic" src={connosieurgif}/></a>
                             <br/>
                             <p> A web application for sneaker enthusiasts to gather and talk about all things sneaker related as well as show off their personal owns and wants. Built with <strong>React.js</strong> and <strong> Ruby on Rails</strong>, for frontend and backend respectively. Used a web scraper wrapped in Node to obtain sneaker data.</p>
-                        <p><strong><a href="https://www.youtube.com/watch?v=PVA6Xyg6c74&t=23s">Video Demo</a><a href="https://github.com/jtx007/ConnosieurFrontEnd">Github</a> | <a href="https://connosieurfrontend.herokuapp.com/">Live</a></strong></p>
+                        <p><strong><a href="https://www.youtube.com/watch?v=PVA6Xyg6c74&t=23s">Video Demo</a> | <a href="https://github.com/jtx007/ConnosieurFrontEnd">Github</a> | <a href="https://connosieurfrontend.herokuapp.com/">Live</a></strong></p>
                             
                         </div>
                         <div className="project animated flipInY delay-4s">
                             <h1>Blitz</h1>
                         <a href="https://blitzweatherapp.herokuapp.com/"><img className="project-picture" alt="demopic" src={blitzgif} /></a>
                             <p>A weather widget created with <strong>React.js</strong> and <strong>Node.js</strong> with <strong>Express</strong>. Used MapQuest geocoder API to convert physical locations into lat/lng coordinates. Used said coordinates for the DarkSky API to obtain relevant weather data to be rendered to the client</p>
-                        <p><strong><a href="https://www.youtube.com/watch?v=e5FhkyYptFE">Video Demo</a><a href="https://github.com/jtx007/BlitzWeatherApp">Github</a> | <a href="https://blitzweatherapp.herokuapp.com/">Live</a></strong></p>
+                        <p><strong><a href="https://www.youtube.com/watch?v=e5FhkyYptFE">Video Demo</a> | <a href="https://github.com/jtx007/BlitzWeatherApp">Github</a> | <a href="https://blitzweatherapp.herokuapp.com/">Live</a></strong></p>
                             
                         </div>
                         <div className="project animated flipInY delay-5s">
