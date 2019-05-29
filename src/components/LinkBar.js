2c9796809ec2254b5dde01e17ca466b92454720d
jtx007 <jamesjacobthomas7@gmail.com>
2019-05-29 15:43:20 -0400

    font changes

--- src/components/LinkBar.js
+++ src/components/LinkBar.js
@@ -8,16 +8,16 @@ export default class LinkBar extends Component {
         return (
             <div className="link-bar animated fadeInDownBig delay-1s"> 
                 <div className="icon-link">
-                    <a href="https://github.com/jtx007"><img className="icon-img" alt="github" src={githubicon}/></a>
-                    <p className="nav-icon-title">Github</p>
+                    <a href="https://github.com/jtx007"><img className="icon-img" alt="github" src={githubicon}/>
+                    <p className="nav-icon-title">Github</p></a>
                 </div>
                 <div className="icon-link">
-                    <a href="https://medium.com/@jamesjacobthomas7"><img className="icon-img" alt="Medium" src={mediumicon} /></a>
-                    <p className="nav-icon-title">Medium</p>
+                    <a href="https://medium.com/@jamesjacobthomas7"><img className="icon-img" alt="Medium" src={mediumicon} />
+                    <p className="nav-icon-title">Medium</p></a>
                 </div>
                 <div className="icon-link">
-                    <a href="https://www.linkedin.com/in/james-thomas007/"><img className="icon-img" alt="Linkedin" src={linkedinicon} /></a>
-                    <p className="nav-icon-title">Linkedin</p>
+                    <a href="https://www.linkedin.com/in/james-thomas007/"><img className="icon-img" alt="Linkedin" src={linkedinicon} />
+                    <p className="nav-icon-title">Linkedin</p></a>
                 </div>
             </div>
         )
