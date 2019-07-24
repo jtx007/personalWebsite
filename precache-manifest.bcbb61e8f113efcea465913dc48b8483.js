4e1a3e128de20aa114874020243b433e3de77715
jtx007 <jamesjacobthomas7@gmail.com>
2019-07-24 15:24:52 -0400

    Updates

---
+++ precache-manifest.bcbb61e8f113efcea465913dc48b8483.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cce797171caf8b015974",
+    "url": "/static/css/main.565e852a.chunk.css"
+  },
+  {
+    "revision": "cce797171caf8b015974",
+    "url": "/static/js/main.cce79717.chunk.js"
+  },
+  {
+    "revision": "45db082a4a4a72e713d0",
+    "url": "/static/js/1.45db082a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36b40c315d802a6db2df66cde4de0af0",
+    "url": "/static/media/connosieur.36b40c31.png"
+  },
+  {
+    "revision": "6901fe1c8900f82e44f10687ec5f04ce",
+    "url": "/static/media/blitz.6901fe1c.png"
+  },
+  {
+    "revision": "9f1e14b2d1e209eea4008ff6e509af30",
+    "url": "/static/media/monogram.9f1e14b2.png"
+  },
+  {
+    "revision": "2302d5fd336bfdc582a6b2245831b7dd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fdf1378b5bc7642085956f22f7037f88.js"
+  "/precache-manifest.bcbb61e8f113efcea465913dc48b8483.js"
 );
 
 workbox.clientsClaim();
