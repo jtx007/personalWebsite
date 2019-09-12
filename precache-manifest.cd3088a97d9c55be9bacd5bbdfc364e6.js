4dbe096e136799a82f5f618c38c3e8ad0a8c5575
jtx007 <jamesjacobthomas7@gmail.com>
2019-09-12 14:11:03 -0400

    Updates

---
+++ precache-manifest.cd3088a97d9c55be9bacd5bbdfc364e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42dc991d0a56a67a97cf154b4643751",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5db1ef2adbc2c02371bf",
+    "url": "/static/css/main.2598ac23.chunk.css"
+  },
+  {
+    "revision": "45e4a91e9ef57e93b91f",
+    "url": "/static/js/2.9d6bce7d.chunk.js"
+  },
+  {
+    "revision": "5db1ef2adbc2c02371bf",
+    "url": "/static/js/main.9b9b7343.chunk.js"
+  },
+  {
+    "revision": "560ba80bb29eed1ad7e1",
+    "url": "/static/js/runtime~main.29359674.js"
+  },
+  {
+    "revision": "6901fe1c8900f82e44f10687ec5f04ce",
+    "url": "/static/media/blitz.6901fe1c.png"
+  },
+  {
+    "revision": "36b40c315d802a6db2df66cde4de0af0",
+    "url": "/static/media/connosieur.36b40c31.png"
+  },
+  {
+    "revision": "d321cbe3166d7fc236c7f3d2fe47106f",
+    "url": "/static/media/digitaljournalnotes.d321cbe3.png"
+  },
+  {
+    "revision": "9f1e14b2d1e209eea4008ff6e509af30",
+    "url": "/static/media/monogram.9f1e14b2.png"
+  }
+]);
